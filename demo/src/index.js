e2df04d85f6e217bd2e18b96c3048143cdbcfd52
rahulravindrancopart <rahul.ravindran@copart.com>
2017-10-09 11:08:11 +0530

    Updated demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RTI from '../../src/withStyles'
 
 class Demo extends Component {
   render() {
     return <div>
       <h1>React Telephone Input Demo</h1>
+      <MuiThemeProvider>
       <RTI
         defaultCountry="in"
         flagsImagePath="./images/flags.png"
       />
+  </MuiThemeProvider>
     </div>
   }
 }
